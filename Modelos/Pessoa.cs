7946f17605a79ccefcbfcd1ba97e391857b304f9
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-06-07 12:59:30 -0300

    Adicionar arquivos de projeto.

---
+++ Modelos/Pessoa.cs
@@ -0,0 +1,14 @@
+﻿namespace ApiGestao.Modelos
+{
+    public class Pessoa
+    {
+        public Pessoa(Guid id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+    }
+}
---
+++ Rotas/PessoaRotas.cs
@@ -0,0 +1,22 @@
+﻿using ApiGestao.Modelos;
+
+namespace ApiGestao.Rotas
+{
+    public static class PessoaRotas
+    {
+        public static List<Pessoa> Pessoas = new List<Pessoa>()
+        {
+            new Pessoa(Guid.NewGuid(), "Neymar"),
+            new Pessoa(Guid.NewGuid(), "Messi"),
+            
+        };
+
+        public static void MapPessoaRotas (this WebApplication app)
+        {
+            app.MapGet("/pessoa", () => Pessoas);
+
+            app.MapGet("/pessoa/{nome}", (string nome) => Pessoas.Find(x => x.Nome == nome));
+
+        }
+    }
+}
